1faef3b4bb5495252284909db4d444925ef4519e
nzai <nzaih18@gmail.com>
2023-01-18 10:58:39 +0300

    explore

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
+import Category from "./pages/Category";
 import PrivateRoutes from "./components/PrivateRoutes";
 
 const App = () => {
@@ -18,9 +19,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoutes />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
+
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
