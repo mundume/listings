e9681e82f9ee3bbaa9d5efac45019fc342d04d64
nzai <nzaih18@gmail.com>
2023-01-20 06:52:37 +0300

    img upload

--- src/pages/CreateListings.jsx
+++ src/pages/CreateListings.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { db } from "../firebase.config";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
+import { async } from "@firebase/util";
 
 const CreateListings = () => {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -56,12 +66,14 @@ const CreateListings = () => {
       isMounted.current = false;
     };
   }, [isMounted]);
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     if (discountedaPrice >= regularPrice) {
       setLoading(false);
-      toast.error("Discounted price needs to be less than the regular price");
+      throw new Error(
+        `${discountedaPrice} is not supposed to be greater than ${regularPrice} `
+      );
       return;
     }
     if (images.length > 6) {
@@ -71,7 +83,7 @@ const CreateListings = () => {
     let geolocation = {};
     let location;
     if (geolocationEnabled) {
-      if (location === undefined || location.includes("undefined")) {
+      if (address) {
         setLoading(false);
         toast.error("enter correct adress");
       }
@@ -80,6 +92,70 @@ const CreateListings = () => {
       geolocation.lng = longitude;
       location = address;
     }
+
+    //store images in firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, "images/" + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+              case "storage/unauthorized":
+                // User doesn't have permission to access the object
+                break;
+              case "storage/canceled":
+                // User canceled the upload
+                break;
+
+              // ...
+
+              case "storage/unknown":
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+            }
+          },
+          () => {
+            // Upload completed successfully, now we can get the download URL
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+    console.log(imgUrls);
+    setLoading(false);
   };
   const onMutate = (e) => {
     let boolean = null;
@@ -237,7 +313,6 @@ const CreateListings = () => {
             id="address"
             value={address}
             onChange={onMutate}
-            required
           />
 
           {!geolocationEnabled && (
